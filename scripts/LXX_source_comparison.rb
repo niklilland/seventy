69e1756c5b7f94f493b475a55be304af0fceeabb
niklaslilland <niklilland@gmail.com>
2020-08-07 15:34:08 -0700

    Wrote new script to parse unbound septuagint and correct duplicate verses as well as missing blank verses.

--- scripts/LXX_source_comparison.rb
+++ scripts/LXX_source_comparison.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
-
 # rubocop:disable all
-
 require 'json'
 
-# Sources for the LXX that need to be compiled into one source:
-# lemma
-# glossary
-# strongs
-# verse reference
-# root document
+
+# The purpose of this script is to compare two different sources of the Septuagint
+# in order to determine if they are the same. If they are, then we can use the extra
+# sources from one with the other (Strong's numbers, glossary definitions, etc.).
+
+
+# - lemma: A file containing the Greek lemma for each word
+# - glossary: A file containing the English translation for each word
+# - strongs: A file containing the Strong's number for each word
+# - verse reference: A file containing the starting word's line for each verse in LXX
+# - root document: A file containing the original Greek word (not lemma) for each word in LXX
 
 verse_filepath = '/Users/niklaslilland/dev/king-json-bible/LXX/LXX-word-to-verse.txt'.freeze
 lemma_filepath = '/Users/niklaslilland/dev/king-json-bible/LXX/LXX-word-to-lemma.txt'.freeze
@@ -17,21 +20,27 @@ glossary_filepath = '/Users/niklaslilland/dev/king-json-bible/LXX/LXX-word-to-gl
 strongs_filepath = '/Users/niklaslilland/dev/king-json-bible/LXX/LXX-word-to-strongs.txt'.freeze
 actual_filepath = '/Users/niklaslilland/dev/king-json-bible/LXX/LXX-root-document.txt'.freeze
 formatted_directory = '/Users/niklaslilland/dev/king-json-bible/LXX/formatted'.freeze
-@ref = { 'Gen' => 'Genesis', 'Exod' => 'Exodus', 'Lev' => 'Leviticus', 'Num' => 'Numbers', 'Deut' => 'Deuteronomy', 'JoshB' => 'Joshua_Codex_Vaticanus', 'JoshA' => 'Joshua_Codex_Alexandrinus', 'JudgB' => 'Judges_Codex_Vaticanus', 'JudgA' => 'Judges_Codex_Alexandrinus', 'Ruth' => 'Ruth', '1Sam' => '1Samuel', '2Sam' => '2Samuel', '1Kgs' => '1Kings', '2Kgs' => '2Kings', '1Chr' => '1Chronicles', '2Chr' => '2Chronicles', '1Esdr' => nil, '2Esdr' => ['Ezra', 'Nehemiah'], 'Esth' => 'Esther', 'Jdt' => nil, 'TobBA' => nil, 'TobS' => nil, '1Macc' => nil, '2Macc' => nil, '3Macc' => nil, '4Macc' => nil, 'Ps' => 'Psalms', 'Odes' => nil, 'Prov' => 'Proverbs', 'Eccl' => 'Ecclesiastes', 'Song' => 'SongOfSongs', 'Job' => 'Job', 'Wis' => nil, 'Sir' => nil, 'PsSol' => nil, 'Hos' => 'Hosea', 'Mic' => 'Micah', 'Amos' => 'Amos', 'Joel' => 'Joel', 'Jonah' => 'Jonah', 'Obad' => 'Obadiah', 'Nah' => 'Nahum', 'Hab' => 'Habakkuk', 'Zeph' => 'Zephaniah', 'Hag' => 'Haggai', 'Zech' => 'Zechariah', 'Mal' => 'Malachi', 'Isa' => 'Isaiah', 'Jer' => 'Jeremiah', 'Bar' => nil, 'EpJer' => nil, 'Lam' => 'Lamentations', 'Ezek' => 'Ezekiel', 'BelOG' => nil, 'BelTh' => nil, 'DanOG' => 'Daniel_Old_Greek', 'DanTh' => 'Daniel_Theodotion', 'SusOG' => nil, 'SusTh' => nil }
-special_handling = ['JoshA', 'JoshB', 'JudgA', 'JudgB', '2Esdr', 'Ps', 'Prov', 'DanOG', 'DanTh']
 
+# A hash of the book title abbreviations found in the source files mapped to their full names
+# Apocryphal books are given value of 'nil' so they are skipped
+@ref = { 'Gen' => 'Genesis', 'Exod' => 'Exodus', 'Lev' => 'Leviticus', 'Num' => 'Numbers', 'Deut' => 'Deuteronomy', 'JoshB' => 'Joshua_Codex_Vaticanus', 'JoshA' => 'Joshua_Codex_Alexandrinus', 'JudgB' => 'Judges_Codex_Vaticanus', 'JudgA' => 'Judges_Codex_Alexandrinus', 'Ruth' => 'Ruth', '1Sam' => '1Samuel', '2Sam' => '2Samuel', '1Kgs' => '1Kings', '2Kgs' => '2Kings', '1Chr' => '1Chronicles', '2Chr' => '2Chronicles', '1Esdr' => nil, '2Esdr' => ['Ezra', 'Nehemiah'], 'Esth' => 'Esther', 'Jdt' => nil, 'TobBA' => nil, 'TobS' => nil, '1Macc' => nil, '2Macc' => nil, '3Macc' => nil, '4Macc' => nil, 'Ps' => 'Psalms', 'Odes' => nil, 'Prov' => 'Proverbs', 'Eccl' => 'Ecclesiastes', 'Song' => 'SongOfSongs', 'Job' => 'Job', 'Wis' => nil, 'Sir' => nil, 'PsSol' => nil, 'Hos' => 'Hosea', 'Mic' => 'Micah', 'Amos' => 'Amos', 'Joel' => 'Joel', 'Jonah' => 'Jonah', 'Obad' => 'Obadiah', 'Nah' => 'Nahum', 'Hab' => 'Habakkuk', 'Zeph' => 'Zephaniah', 'Hag' => 'Haggai', 'Zech' => 'Zechariah', 'Mal' => 'Malachi', 'Isa' => 'Isaiah', 'Jer' => 'Jeremiah', 'Bar' => nil, 'EpJer' => nil, 'Lam' => 'Lamentations', 'Ezek' => 'Ezekiel', 'BelOG' => nil, 'BelTh' => nil, 'DanOG' => 'Daniel_Old_Greek', 'DanTh' => 'Daniel_Theodotion', 'SusOG' => nil, 'SusTh' => nil }
+# List of books that require 'special handling', e.g. their sources in the LXX text don't match KJV exactly
+special_handling = ['JoshA', 'JoshB', 'JudgA', 'JudgB', '2Esdr', 'Ps', 'Prov', 'DanOG', 'DanTh']
+# Hash containing the destination files for each book to be written to
 dest_io = {}
 
-# # Create a file for each of the books
-# @ref.each do |key, value|
-#   dest_io[key] = File.new("/Users/niklaslilland/dev/king-json-bible/LXX/formatted/#{value}.json", 'w+') unless value.nil?
-# end
-# dest_io['Ezra'] = File.new('/Users/niklaslilland/dev/king-json-bible/LXX/formatted/Ezra.json', 'w+')
-# dest_io['Nehemiah'] = File.new('/Users/niklaslilland/dev/king-json-bible/LXX/formatted/Nehemiah.json', 'w+')
-# dest_io['Esther_With_Additions'] = File.new('/Users/niklaslilland/dev/king-json-bible/LXX/formatted/Esther_With_Additions.json', 'w+')
+# Create a output file for each of the books, unless it's apocryphal, or Esdras 2 (which is actually Ezra and Nehemiah)
+@ref.each do |key, value|
+  dest_io[key] = File.new("/Users/niklaslilland/dev/king-json-bible/LXX/formatted/#{value}.json", 'w+') unless (value.nil? || value === '2Esdr')
+end
+# Due to source issues, Ezra and Nehemiah are created manually, and an additional file is created for the additional text found in Esther
+dest_io['Ezra'] = File.new('/Users/niklaslilland/dev/king-json-bible/LXX/formatted/Ezra.json', 'w+')
+dest_io['Nehemiah'] = File.new('/Users/niklaslilland/dev/king-json-bible/LXX/formatted/Nehemiah.json', 'w+')
+dest_io['Esther_With_Additions'] = File.new('/Users/niklaslilland/dev/king-json-bible/LXX/formatted/Esther_With_Additions.json', 'w+')
 
 
-# Open reference files
+
+# Open each source file and set the lineno to 1
 @lemma_file = File.open(lemma_filepath, 'r')
 @glossary_file = File.open(glossary_filepath, 'r')
 @strongs_file = File.open(strongs_filepath, 'r')
@@ -46,10 +55,8 @@ dest_io = {}
 @current_book = 'Gen' # init with first book value
 @current_chapter = 1
 @current_verse = 1
-apocryphal_skip = 0
-special_skip = 0
 
-# Open verse file to iterate over
+# Open source file with starting line numbers for each verse. This is the 'main' source file.
 verse_file = File.open(verse_filepath, 'r')
 current_line = verse_file.gets
 
@@ -126,38 +133,54 @@ def log_value(write, verse_container, copy)
   end
 end
 
-def transition_book(book)
+def write_book_to_file(book)
   # New book, write @processed to file then clear it
   # TODO: Undo this
   # TODO: Special handling for Ezra and Nehemiah
   # TODO: Special handling for Esther and Esther_With_Additions
   # dest_io[@current_book].write(@processed.to_json)
   # TODO: Close the io?
-  puts "Wrote #{@ref[@current_book]} to file!\n"
+  puts "Completed processing #{@ref[@current_book]}!\n"
   # TODO: Change these to match the KVJ values
   @processed = { :verses => [], :additional => [], :id => @ref[book], :title => @ref[book] }
   @current_book = book
 end
 
 
-# 30637 lines in file, this is shorthand for iterate over every verse
+# Iterate over every verse in the source file
 while verse_file.lineno <= 30637
 
+  # Parse book abbreviation, chapter, verse, and line number (not all lines are this clean, but this is a good guide)
   next_line = verse_file.gets
   split = current_line.split('.')
-  # Parse book shorthand from line
   book = split[0]
   chapter = split[1]
   verse = split[2].split('-')[0]
   line = split[2].split('-')[1].to_i
 
-  # If we've reached the end of the file
+  # If the new line is a new book, write the book object to file and wipe the @processed variable, then close that book's io
+  write_book_to_file(book) if book != @current_book
+
+  # If the current verse is from an apocryphal book, skip it.
+  if @ref[book].nil?
+    # Determine number of words needed to be skipped in other files
+    words_to_skip = line - @actual_file.lineno
+    "Skipping #{words_to_skip} words in #{book} #{chapter}:#{verse} (apocryphal book)"
+    words_to_skip.times do |i|
+      @actual_file.gets
+      @glossary_file.gets
+      @lemma_file.gets
+      @strongs_file.gets
+    end
+    next
+  end
+
+  # calculate how many words are in the verse
   if next_line.nil?
     puts 'END OF THE VERSE FILE'
-    # hard-code length of the last file
+    # hard-code length of the last line
     words_in_verse = 13
   else
-    # calculate how many lines the reference files need to move forward to match the next verse
     next_verse_start = next_line.split('-')[1].to_i
     if next_line == "DanOG.5.26-28-606213\n"
       next_verse_start = next_line.split('-')[2].to_i
@@ -165,44 +188,53 @@ while verse_file.lineno <= 30637
     words_in_verse = next_verse_start - line
   end
 
-  # TODO: Come back to this at a later time and process the apocryphal books as well
-  # Skip if the ref is nil, e.g. skip if it's an Apocryphal book, but still increase the line numbers
-  if @ref[book].nil?
-    apocryphal_skip += 1
-  elsif special_handling.include?(book)
 
-    if apocryphal_skip > 0
-      skip = line - @actual_file.lineno
-      puts "Skipping #{skip} lines in reference files (apocryphal) - #{book} #{chapter}:#{verse}"
-      # 'line' is the line number that the reference files need to be iterated to
-      skip.times do |i|
-        @actual_file.gets
-        @glossary_file.gets
-        @lemma_file.gets
-        @strongs_file.gets
-      end
-      apocryphal_skip = 0
-    end
 
-    if special_skip > 0
-      skip = line - @actual_file.lineno
-      puts "Skipping #{skip} lines in reference files (special handling) - #{book} #{chapter}:#{verse}"
-      # 'line' is the line number that the reference files need to be iterated to
-      skip.times do |i|
-        @actual_file.gets
-        @glossary_file.gets
-        @lemma_file.gets
-        @strongs_file.gets
-      end
-      special_skip = 0
-    end
+  # TODO: Problems to handle:
+  # - books with lettered verses
+  # - books with verse ranges
+  # - books with skipped verses
+  # - alternate versions of books
+  # - books with chapters and verses out of order
+
+  # Books with lettered verses
+  # JoshB
+  # 1Kgs
+  # 2Chr
+  # Esth
+  # Prov
+  # Job
+
+  # Books with verse spans
+  # DanOG
+
+  # Books with alternate versions
+  # JoshA
+  # JoshB
+  # JudgA
+  # JudgB
+  # 2Esdr
+  # DanOG
+  # DanTh
+
+  # Other source issues:
+  # Ps
+  # Prov
+
+
+
+
+  # -------------------------------------------------------------------------------------
+  # -------------------- Actual processing of the verse is done here --------------------
+  # -------------------------------------------------------------------------------------
+
+  # If books require special handling, handle it in this block
+  if special_handling.include?(book)
 
     # If book is the same
     # if chapter is the same
     # but verse is not current_verse + 1
     # insert verses until it is
-    # 
-
     if book == @current_book
       if chapter.to_i == @current_chapter
         if verse.to_i == @current_verse
@@ -211,28 +243,14 @@ while verse_file.lineno <= 30637
           # verse matches what it SHOULD be, don't insert anything
         else
           # verse is not the same, is not next value, so we need to insert n verses
-
           # How do we determine what n is?
-
           (verse.to_i - @current_verse).times do |i|
             process_verse(@ref[book], chapter, verse, 0, chapter, verse)
           end
-
         end
-
-
       end
-
-
     end
 
-
-
-    # If the book changes, write the book object to file and wipe the @processed variable, then close the io!
-    if book != @current_book
-      # New book, write @processed to file then clear it
-      transition_book(book)
-    end
 
     case book
     when 'JoshA'
@@ -253,10 +271,26 @@ while verse_file.lineno <= 30637
       end
     when 'Ps'
       # TODO: Map out verse reassignment
-      special_skip += 1
+      # Because we haven't implemented the special handling for this book, skip the verse
+      words_to_skip = line - @actual_file.lineno
+      puts "Skipping #{words_to_skip} words in #{book} #{chapter}:#{verse} (requires special handling, not yet implemented)"
+      words_to_skip.times do |i|
+        @actual_file.gets
+        @glossary_file.gets
+        @lemma_file.gets
+        @strongs_file.gets
+      end
     when 'Prov'
       # TODO: Look into what needs to change
-      special_skip += 1
+      # Because we haven't implemented the special handling for this book, skip the verse
+      words_to_skip = line - @actual_file.lineno
+      puts "Skipping #{words_to_skip} words in #{book} #{chapter}:#{verse} (requires special handling, not yet implemented)"
+      words_to_skip.times do |i|
+        @actual_file.gets
+        @glossary_file.gets
+        @lemma_file.gets
+        @strongs_file.gets
+      end
     when 'DanOG'
       if chapter.to_i == 3
         # if verse is 24-90, not kjv
@@ -333,69 +367,17 @@ while verse_file.lineno <= 30637
         process_verse('Daniel (Theodotion)', chapter, verse, words_in_verse, chapter, verse)
       end
     end
-  else
-    if apocryphal_skip > 0
-      skip = line - @actual_file.lineno
-      puts "Skipping #{skip} lines in reference files (apocryphal) - #{book} #{chapter}:#{verse}"
-      # 'line' is the line number that the reference files need to be iterated to
-      skip.times do |i|
-        @actual_file.gets
-        @glossary_file.gets
-        @lemma_file.gets
-        @strongs_file.gets
-      end
-      apocryphal_skip = 0
-    end
 
-    if special_skip > 0
-      skip = line - @actual_file.lineno
-      puts "Skipping #{skip} lines in reference files (special handling) - #{book} #{chapter}:#{verse}"
-      # 'line' is the line number that the reference files need to be iterated to
-      skip.times do |i|
-        @actual_file.gets
-        @glossary_file.gets
-        @lemma_file.gets
-        @strongs_file.gets
-      end
-      special_skip = 0
-    end
+  else
 
     # If the book changes, write the book object to file and wipe the @processed variable, then close the io!
     if book != @current_book
       # New book, write @processed to file then clear it
-      transition_book(book)
+      write_book_to_file(book)
     end
 
     process_verse(book, chapter, verse, words_in_verse, chapter, verse)
   end
+
   current_line = next_line
 end
-
-
-# # Goal format
-# {
-#   "book": "1 John"
-#   "chapter": 1,
-#   "verse": 1,
-#   "kjv_chapter": 1,
-#   "kjv_verse": 1,
-#   "verse_letter": 'a', # For additional apocryphal verses
-#   "text": "That which was from the beginning, which we have heard, which we have seen with our eyes, which we have looked upon, and our hands have handled, of the Word of life;",
-#   "reference": [
-#     {
-#       "index": 0,
-#       "strongs": "G2455",
-#       "text": "Jude,"
-#     },
-#     {
-#       "index": 1,
-#       "strongs": 0,
-#       "text": "the"
-#     },
-#     {
-#       "index": 2,
-#       "strongs": "G1401",
-#       "text": "servant"
-#     }
-#   ]
-# }
---
+++ scripts/unbound/transform_unbound_LXX.rb
@@ -0,0 +1,136 @@
+# frozen_string_literal: true
+# rubocop:disable all
+require 'json'
+
+# This script transforms the Unbound LXX sources into a more workable format.
+# This is done by re-ordering some verses that are out of order and removing
+# the gramatical codes.
+
+actual_path = '/Users/niklaslilland/dev/king-json-bible/LXX/unbound-source/_actual-source.txt'
+lemma_path = '/Users/niklaslilland/dev/king-json-bible/LXX/unbound-source/_lemma-source-parsed.txt'
+book_code_path = '/Users/niklaslilland/dev/king-json-bible/LXX/unbound-source/_book-map.txt'
+
+trans_actual_path = '/Users/niklaslilland/dev/king-json-bible/LXX/unbound-transformed/transformed-actual-source.txt'
+trans_lemma_path = '/Users/niklaslilland/dev/king-json-bible/LXX/unbound-transformed/transformed-lemma-source.txt'
+
+manual = {
+  '01O' => {
+    '31' => "01O\t31\t51\t01O\t31\t51\t\t0\t\n",
+    '35' => "01O\t35\t21\t01O\t35\t\t\t0\t\n"
+  }
+}
+
+@actual_file = File.open(actual_path, 'r')
+@trans_actual = File.new(trans_actual_path, 'w+')
+@book_file = File.open(book_code_path, 'r')
+
+# Create a hash of the book codes with their names
+@book_map = {}
+@book_file.each do |line|
+  parse = line.split("\t")
+  @book_map[parse[0]] = parse[1].chomp
+end
+
+# skip intro lines
+9.times { @trans_actual.write(@actual_file.gets) }
+
+# Local variables
+@verse_tracker = {}
+@sept_index = 0
+@prev_book = nil
+@prev_chapter = nil
+@prev_verse = nil
+@prev_subverse = nil
+file_length = 30041
+
+def insert_verses(book, chapter, next_verse, subverse, prev_verse)
+  # If the previous verse is greater than the next verse (e.g. 25, then 5), fill in starting from 1
+  # else fill in starting at prev_verse
+  in_verse = prev_verse > next_verse ? 1 : prev_verse + 1
+  puts "INSERT #{@book_map[book]} #{chapter}:#{in_verse}"
+  @trans_actual.write("#{book}\t#{chapter}\t#{in_verse}\t#{book}\t#{chapter}\t\t\t0\t\n")
+  @verse_tracker[book][chapter].push("#{in_verse}")
+  insert_verses(book, chapter, next_verse, subverse, in_verse) if in_verse + 1 < next_verse
+end
+
+# Writes verse to output file, records verse as tracked in the @verse_tracker variable
+def process_verse(book, chapter, verse, subverse, input)
+  @trans_actual.write(input)
+  @verse_tracker[book][chapter].push("#{verse}#{subverse}")
+end
+
+
+while @actual_file.lineno <= file_length
+  input = @actual_file.gets
+
+  # Parse the reference values from line
+  references = input.split("\t")
+  book = references[0]
+  chapter = references[1]
+  verse = references[2]
+  sept_book = references[3]
+  sept_chapter = references[4]
+  sept_verse = references[5]
+  sept_subverse = references[6]
+  sept_index = references[7]
+  sept_text = references[8]
+
+  # Skip the verses that are duplicated
+  if sept_index == @sept_index
+    next
+  end
+  @sept_index = sept_index
+
+  # If the sept_book changes, add the sept_book to the tracker with the current sept_chapter set with an empty array
+  if @verse_tracker[sept_book].nil?
+    @verse_tracker[sept_book] = { sept_chapter => [] }
+  # If the chapter changes, add the chapter to the tracker with an empty array
+  elsif @verse_tracker[sept_book][sept_chapter].nil?
+    @verse_tracker[sept_book][sept_chapter] = []
+  end
+
+
+  # If verse has book/chapter/verse/book/chapter/no verse, it's a filler verse, skip it
+  if input.match(/\d+[ONA]{1}\t\d+\t\d+\t\d+[ONA]{1}\t\d+\t\t\t/)
+    next
+  else
+    # If missing verses at start of chapter, insert verses
+    if @verse_tracker[sept_book][sept_chapter].empty? && sept_verse.to_i != 1
+      # If there is a subverse value
+      if sept_subverse != ''
+        # It's a new chapter, the verse we're processing is not 1, and there's a subverse.
+        # Can this even happen?
+        puts 'TEST VALUE FINDME'
+      else
+        # First n verses are missing in chapter, insert them
+        insert_verses(sept_book, sept_chapter, sept_verse.to_i, sept_subverse, @prev_verse)
+        process_verse(sept_book, sept_chapter, sept_verse, sept_subverse, input)
+      end
+
+
+    # # If the verse is the same as the previous, and the subverse value of current or last isn't empty, it's a subverse!
+    # elsif (@verse_tracker[sept_book][sept_chapter][-1].to_i == sept_verse.to_i) && (sept_subverse != '' || @prev_subverse != '')
+    #   puts 'subverse'
+    #   process_verse(sept_book, sept_chapter, sept_verse, sept_subverse, input)
+  
+    # If the current verse is not the last verse processed + 1, we're missing n verses, grab them from @buffer
+    elsif @verse_tracker[sept_book][sept_chapter][-1].to_i + 1 != sept_verse.to_i
+      # Get n values from @buffer, then process current verse
+      insert_verses(sept_book, sept_chapter, sept_verse.to_i, sept_subverse, @prev_verse)
+      process_verse(sept_book, sept_chapter, sept_verse, sept_subverse, input)
+    else
+      process_verse(sept_book, sept_chapter, sept_verse, sept_subverse, input)
+    end
+  end
+
+  puts "#{@book_map[sept_book]} #{sept_chapter}:#{sept_verse}#{sept_subverse}"
+
+  @prev_book = sept_book
+  @prev_chapter = sept_chapter.to_i
+  @prev_verse = sept_verse.to_i
+  @prev_subverse = sept_subverse
+end
+
+@actual_file.close()
+@trans_actual.close()
+@book_file.close()
